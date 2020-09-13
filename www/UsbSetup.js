5ae1524f2e83f25dd27599d99ec47e020ba8a945
decute <manish@anom.io>
2020-09-13 22:51:43 +0530

    method added

--- www/UsbSetup.js
+++ www/UsbSetup.js
@@ -1,13 +1,10 @@
 var exec = require('cordova/exec');
 
-module.exec.coolMethod = function (arg0, success, error) {
+module.exports.coolMethod = function (arg0, success, error) {
     exec(success, error, 'UsbSetup', 'coolMethod', [arg0]);
 };
 
 //This will give information about the list of device connected too
-module.exec.findUsbDevices = function (success, error) {
+module.exports.findUsbDevices = function (success, error) {
     exec(success, error, 'UsbSetup', 'findUsbDevices', []);
 };
-
-
-
